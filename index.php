28fbb13087dda3f7866d80d85db9a236d0ebaeff
reekendra yadav <reekendray461@gmail.com>
2018-12-16 14:08:49 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coding-Cognition</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<form action="includes/signup.inc.php" method="POST">
+    <input type="text" name="first" placeholder="FirstName">
+    <br>
+    <input type="text" name="last" placeholder="LastName">
+    <br>
+    <input type="text" name="email" placeholder="E-mail">
+    <br>
+    <input type="text" name="uid" placeholder="UserName">
+    <br>
+    <input type="password" name="pwd" placeholder="Password">
+    <br>
+    <button type="submit" name="submit">Sign up</button>
+</form>
+
+
+</body>
+</html>
